a7b38edb1fd23a513884ff0ac5e3d9b2e760efec
Calvin <calvintapp@gmail.com>
2018-12-05 09:05:42 +0000

    PowerShell scripts
    PowerShell Scripts

---
+++ SnipsSolution/Snips.Test/PowerShell/Common.ps1
@@ -0,0 +1,22 @@
+<#
+.SYNOPSIS
+Collection of functions that can be used for various usages.
+#>
+function Get-UDVariable {
+    get-variable | where-object {(@(
+      "FormatEnumerationLimit",
+      "MaximumAliasCount",
+      "MaximumDriveCount",
+      "MaximumErrorCount",
+      "MaximumFunctionCount",
+      "MaximumVariableCount",
+      "PGHome",
+      "PGSE",
+      "PGUICulture",
+      "PGVersionTable",
+      "PROFILE",
+      "PSSessionOption"
+      ) -notcontains $_.name) -and `
+      (([psobject].Assembly.GetType('System.Management.Automation.SpecialVariables').GetFields('NonPublic,Static') | Where-Object FieldType -eq ([string]) | ForEach-Object GetValue $null)) -notcontains $_.name
+      }
+  }
