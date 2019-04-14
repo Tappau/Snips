69e9c84e9921f22a5f217991af5c4c68e905bf58
Calvin Tapp <calvintapp@outlook.com>
2019-04-14 23:54:41 +0100

    Removal of PS scripts into own repo

--- PowerShell/Common/Public/Show-ConfirmationPrompt.ps1
+++
@@ -1,9 +0,0 @@
-function Show-ConfirmationPrompt {
-    param (
-        [string]$Message
-    )
-    do {
-        $reply = Read-Host -Prompt ($Message + " (Y/N) ?")
-    } until ("y","Y","n","N" -contains $reply)
-    return ("y", "Y" -contains $reply)
-}
--- PowerShell/Common/Public/Test-UncPath.ps1
+++
@@ -1,7 +0,0 @@
-function Test-UncPath {
-    param (
-        [Parameter(Mandatory=$true)]
-        $Path
-    )
-    if($Path.StartsWith("\\")){ return $true } else { return $false }    
-}
